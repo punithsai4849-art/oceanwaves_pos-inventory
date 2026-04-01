3463c087c763d01bd5672f8875b9b53dce7ea3f7
punith sai <punith@puniths-MacBook-Air.local>
2026-04-01 17:19:20 +0530

    remove ssl from db options

--- oceanwaves_project/settings.py
+++ oceanwaves_project/settings.py
@@ -70,10 +70,9 @@ DATABASES = {
         'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', default='3306'),
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-            'ssl': {'ca': config('MYSQL_SSL_CA', default='')},
-        }
+    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+    'charset': 'utf8mb4',
+}
     }
 }
 
